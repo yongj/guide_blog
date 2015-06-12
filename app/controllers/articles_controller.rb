93302d1bdcdca8cb58197e9e06b7140df4ded691
Yong Jiang <yong.j.jiang@gmail.com>
2015-06-12 00:45:31 -0700

    complete section 6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,37 @@ class ArticlesController < ApplicationController
   	end
 
 	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
 	  @article = Article.new(article_params)
 	 
-	  @article.save
-	  redirect_to @article
+	  if @article.save
+	  	redirect_to @article
+	  else
+	  	render 'new'
+	  end
+	end
+
+	def update
+		@article = Article.find(param[:id])
+
+		if @article.update(article_params)
+			redirect_to @articleelse
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 	 
 	private
